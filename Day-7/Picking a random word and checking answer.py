7af9627cbe750c50813815ca8a096f8ebce59da5
Chisato-Nakamura <chisato.nakamura@uts.edu.au>
2023-01-11 17:45:21 +1100

    Initial commit

--- Day-7/Picking a random word and checking answer.py
+++ Day-7/Picking a random word and checking answer.py
@@ -4,7 +4,6 @@ word_list = ["aardvark", "baboon", "camel"]
 
 #Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = random.choice(word_list)
-print(chosen_word)
 
 #Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 while True:
@@ -14,7 +13,8 @@ while True:
         break
 
 #Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-if guess in chosen_word:
-    print("Matched")
-else:
-    print("Doesn't match")
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
