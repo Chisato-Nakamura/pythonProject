a66f61d0347af1b1eff2f02c679f9c23117522bf
chisato0901 <chisato.nakamura@uts.edu.au>
2023-02-17 13:56:15 +1100

    Initial commit

--- Day-2/Tip Calculator.py
+++ Day-2/Tip Calculator.py
@@ -5,20 +5,17 @@
 
 
 print("Welcome to the tip calculator.")
-total = input("What was the total bill? $")
-tip = input("What percentage tip would you like to give? 10, 12, or 15? ")
-people = input("How many pople to split the bill? ")
+total = float(input("What was the total bill? $"))
+tip = float(input("What percentage tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many pople to split the bill? "))
 
-float_total = float(total)
-float_tip = float(tip)
-int_people = int(people)
 
 #get the percentage of tip
-if float_tip == 10:
+if tip == 10:
   new_tip = 10/100
-elif float_tip == 12:
+elif tip == 12:
   new_tip = 12/100
-elif float_tip == 15:
+elif tip == 15:
   new_tip = 15/100
 else:
   print("Please select either 10%, 12% or 15%")
@@ -26,5 +23,5 @@ else:
 tip_percentage = 1 + new_tip #either 1.1, 1.12or 1.15
 
 #Total
-new_total = round((float_total / int_people) * tip_percentage, 2)
+new_total = round((total / people) * tip_percentage, 2)
 print(f"Each person should pay: ${new_total}")
